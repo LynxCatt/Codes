3bcc6389960b8b209785de8b2ba95b494311b10c
LynxCat <cheeseocelot@outlook.com>
2023-12-15 08:56:02 +0000

    AC 最短路上的点

--- 最短路上的点.cpp
+++ 最短路上的点.cpp
@@ -1,54 +1,32 @@
 #include <bits/stdc++.h>
-#define pack {v, head[u]}
 using namespace std;
 using ll = long long;
 
-int n, m;
-struct node{
-	int to, nxt;
-}g[1000010];
-int head[2000010], cnt;
+int n, m, q;
+int dis[110][110];
 
-void add(int u, int v) {
-	g[++cnt] = pack;
-	head[u] = cnt;
-}
-
-int dis[1000010];
-int tot[1000010];
-
-void bfs() {
+int main() {
+	// freopen("std.in", "r", stdin);
 	memset(dis, 0x3f, sizeof dis);
-	queue<int> q;
-	q.push(1);
-	dis[1] = 0;
-	while (!q.empty()) {
-		int u = q.front();
-		q.pop();
-		for (int i = head[u]; i; i = g[i].nxt) {
-			int v = g[i].to;
-			if (dis[v] == dis[u]+1) {
-				tot[v] = (tot[v]+tot[u])%100003;
-			} else if (dis[v] == 0x3f3f3f3f) {
-				tot[v] = tot[u];
-				dis[v] = dis[u]+1;
-				q.push(v);
+	scanf("%d%d", &n, &m);
+	for (int u, v, i = 1; i <= m; i++) {
+		scanf("%d%d", &u, &v);
+		dis[u][v] = dis[v][u] = 1;
+	}
+	for (int k = 1; k <= n; k++) {
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				dis[i][j] = min(dis[i][j], dis[i][k]+dis[k][j]);
 			}
 		}
 	}
-}
-
-int main() {
-	scanf("%d%d", &n, &m);
-	for (int i = 1; i <= m; i++) {
-		int u, v;
+	scanf("%d", &q);
+	for (int u, v, i = 1; i <= q; i++) {
 		scanf("%d%d", &u, &v);
-		if (u == v) continue;
-		add(u, v), add(v, u);
-	}
-	tot[1] = 1;
-	bfs();
-	for (int i = 1; i <= n; i++) {
-		printf("%d\n", tot[i]);
+		int ans = 2;
+		for (int k = 1; k <= n; k++) {
+			if (dis[u][k] + dis[k][v] == dis[u][v]) ans++;
+		}
+		printf("%d\n", ans);
 	}
 }
